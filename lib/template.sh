cac905ba8890191355327ca63297dbc75b61df18
Vyacheslav Dryamov <Vyacheslav.Dryamov@infowatch.com>
2026-05-23 08:04:05 +0300

    Clean legacy files after history merge

--- lib/template.sh
+++
@@ -1,6 +0,0 @@
-  GNU nano 7.2                            lib/logger.sh                                      
-#!/bin/bash
-# lib/template.sh
-
-# Защита от повторного импорта
-[[ -n "$_LIB_LOGGER_LOADED" ]] && return || readonly _LIB_LOGGER_LOADED=1
